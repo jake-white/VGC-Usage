e5673fe9eded30a383bcc6e933f6d6f626090a69
Jake White <squirrelboy1225@gmail.com>
2018-01-10 05:20:21 -0500

    Deleted debug output

--- js/parse.js
+++ js/parse.js
@@ -1,3 +1,20 @@
+window.addEventListener('load', function(e) {
+
+  window.applicationCache.addEventListener('updateready', function(e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+      // Browser downloaded a new app cache.
+      // Swap it in and reload the page to get the new hotness.
+      window.applicationCache.swapCache();
+      if (confirm('A new version of this site is available. Load it?')) {
+        window.location.reload();
+      }
+    } else {
+      // Manifest didn't changed. Nothing new to server.
+    }
+  }, false);
+
+}, false);
+
 var names = [], usages = [];
 
 var moves = [], abilities = [], items = [];
@@ -58,7 +75,6 @@ var parseMons = function(data){
     .split('totem').join('')
     .split('.').join('')
     .split('\'').join(''); //getting rid of weird forme bs
-    console.log(shorten + " " + i);
     if(shorten.includes("-alola")){
       shorten = shorten.substring(0,
       shorten.indexOf("-alola"));
