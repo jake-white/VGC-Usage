3eea234b44ecac8fdbd4e0afc18a7dbde8854dc9
Jake White <squirrelboy1225@gmail.com>
2018-01-10 05:26:10 -0500

    Deleted caching stuff
    Turns out github-pages was just down

--- js/parse.js
+++ js/parse.js
@@ -1,20 +1,3 @@
-window.addEventListener('load', function(e) {
-
-  window.applicationCache.addEventListener('updateready', function(e) {
-    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-      // Browser downloaded a new app cache.
-      // Swap it in and reload the page to get the new hotness.
-      window.applicationCache.swapCache();
-      if (confirm('A new version of this site is available. Load it?')) {
-        window.location.reload();
-      }
-    } else {
-      // Manifest didn't changed. Nothing new to server.
-    }
-  }, false);
-
-}, false);
-
 var names = [], usages = [];
 
 var moves = [], abilities = [], items = [];
